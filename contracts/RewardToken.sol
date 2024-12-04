23aa3e083a62c39e56092a5e2a7ae78eda0a0713
Bark Ruffalo <hype@trulyadog.com>
2024-12-04 19:59:33 +0100

    add events for mint and burn

--- contracts/RewardToken.sol
+++ contracts/RewardToken.sol
@@ -5,14 +5,19 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract RewardToken is ERC20, Ownable {
+	event TokensMinted(address indexed to, uint256 amount);
+	event TokensBurned(address indexed from, uint256 amount);
+
 	constructor() ERC20("DRUGS", "DRUGS") Ownable(msg.sender) {}
 
 	function mint(address to, uint256 amount) external onlyOwner {
 		_mint(to, amount);
+		emit TokensMinted(to, amount);
 	}
 
 	function burn(uint256 amount) external {
 		_burn(msg.sender, amount);
+		emit TokensBurned(msg.sender, amount);
 	}
 
 	function burnFrom(address account, uint256 amount) external {
@@ -23,5 +28,6 @@ contract RewardToken is ERC20, Ownable {
 		);
 		_approve(account, msg.sender, currentAllowance - amount);
 		_burn(account, amount);
+		emit TokensBurned(account, amount);
 	}
 }
